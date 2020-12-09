5beb39f9c292ea92deef21c6e4f55f72dd4dd82f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-09 19:05:41 +0000

    Manually created vue file

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'gagan',
+          name: 'Gagandeep Singh',
+          phone: '9876543210',
+          email: 'singh.gagandeep3911@gmail.com',
+        },
+        {
+          id: 'troye',
+          name: 'Troye Sivan',
+          phone: '9988776655',
+          email: 'test@mail.com',
+        },
+      ],
+    };
+  },
+};
+</script>
