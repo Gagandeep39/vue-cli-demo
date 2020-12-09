9c04255daca600e41a48956b96c3a78e00e97750
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-09 19:18:34 +0000

    Added frriend contact

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? 'Hide Details' : 'Show Details' }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'gagan',
+        name: 'Gagandeep Singh',
+        phone: '9876543210',
+        email: 'singh.gagandeep@gmail.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
